d422597254ec3e62efb7bc962088e4b4623cab03
LordCodex <adenirandaniel565@gmail.com>
2024-09-13 21:32:11 +0100

    updated the response of the root route

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const limiter = rateLimit({
 })
 app.use(limiter);
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('<h1>Welcome to the E-commerce API</h1> <h2>API Endpoints</h2>');
 });
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/products', isAuth, product);
