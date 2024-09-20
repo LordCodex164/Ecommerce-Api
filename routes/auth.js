4a78ded22c56992d6cf5f1fedcb8d3e9aa1a6967
LordCodex <adenirandaniel565@gmail.com>
2024-09-20 12:24:45 +0100

    added verify email route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {register, login} = require('../controllers/Auth');
+const {register, login, verifyEmail} = require('../controllers/Auth');
 
 router.post('/login', login);
 router.post('/register', register);
+router.post("/verify_email", verifyEmail);
 
 module.exports = router;
